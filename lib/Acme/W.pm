cb6ac28f549ca905706854b5f9621d9d5b93c157
Dame Ninngenn <dameninngenn.owata@gmail.com>
2010-10-19 21:04:16 +0900

    initial commit

---
+++ lib/Acme/W.pm
@@ -0,0 +1,32 @@
+package Acme::W;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::W -
+
+=head1 SYNOPSIS
+
+  use Acme::W;
+
+=head1 DESCRIPTION
+
+Acme::W is
+
+=head1 AUTHOR
+
+Takashi Higashigata E<lt>dameo@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
